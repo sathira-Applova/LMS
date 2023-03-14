de217b93d3d9fe1f8c757360eacc35e149842ba5
sathira <sathira.w@getApplova.com>
2023-03-14 08:49:55 +0530

    change getDataSource() method to static so that it can be reused by UserDAOI

--- src/main/java/org/userwebapp/service/BooksDAOI.java
+++ src/main/java/org/userwebapp/service/BooksDAOI.java
@@ -20,7 +20,7 @@ public class BooksDAOI implements BooksDAO {
         jdbcTemplate = new JdbcTemplate(ds);
     }
 
-    public DataSource getDataSource() {
+    public static DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/lms_db");
